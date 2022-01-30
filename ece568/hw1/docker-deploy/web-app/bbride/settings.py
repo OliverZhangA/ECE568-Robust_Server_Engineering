01a42876cc86b6568674070a8591e6eccb8c6262
Your Name <you@example.com>
2022-01-30 11:35:17 -0500

    after docker

--- ece568/hw1/docker-deploy/web-app/bbride/settings.py
+++ ece568/hw1/docker-deploy/web-app/bbride/settings.py
@@ -77,13 +77,23 @@ WSGI_APPLICATION = 'bbride.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'bbride',
+#         'PASSWORD': "oliver",
+#         'USER': 'oliver',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bbride',
-        'PASSWORD': "oliver",
-        'USER': 'oliver',
-        'HOST': '127.0.0.1',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
@@ -143,3 +153,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ece568568@gmail.com'
 EMAIL_HOST_PASSWORD = 'dpjimrijcfwyxqzk'  
+
+CSRF_TRUSTED_ORIGINS = ['https://*.mydomain.com','https://*.127.0.0.1','http://vcm-24561.vm.duke.edu:8000']
