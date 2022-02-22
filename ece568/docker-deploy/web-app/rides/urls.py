5e98a50a2e96aae4fb3b4d1eb8aad2653031cece
Your Name <you@example.com>
2022-02-22 12:36:19 -0500

    1.0

--- ece568/docker-deploy/web-app/rides/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='ride-home'),
-    path('owner/', views.owner, name='ride-owner'),
-    path('sharer/', views.sharer, name='ride-sharer'),
-    path('driver/', views.driver, name='ride-driver'),
-]
